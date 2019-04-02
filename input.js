ab671d4d978b7025de94cfe1940723c8b8e22bc9
Wesley Berry <wesleyberry52@gmail.com>
2019-04-02 13:42:08 -0500

    added dependency

--- input.js
+++ input.js
@@ -2,3 +2,12 @@ var inquirer = require("inquirer");
 var createBid = require("createBid.js");
 var makeItem = require("makeItem.js");
 var mysql = require("mysql");
+
+function login() {
+    console.log("----------Username and Password Creation-------------");
+    .inquirer.prompt([
+        {
+            
+        }
+    ])
+}
