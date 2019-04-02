66591c7456a57fd50b487201014a60c0d3e251f7
Wesley Berry <wesleyberry52@gmail.com>
2019-04-02 14:33:37 -0500

    inquirer

--- input.js
+++ input.js
@@ -1,4 +1,39 @@
 var inquirer = require("inquirer");
-var createBid = require("createBid.js");
-var makeItem = require("makeItem.js");
+// var createBid = require("createBid.js");
+// var makeItem = require("makeItem.js");
 var mysql = require("mysql");
+
+var users= [];
+
+function login() {
+    console.log("----------Username and Password Creation-------------");
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter a username:\n",
+            name: "username",
+            validate: function(input) {
+                var done = this.async();
+                setTimeout(function() { 
+                    for(var i = 0; i < users.length; i++) {
+                        if(input === users[i]) {
+                            done("Sorry, that username already exists!\n");
+                            return;
+                        } 
+                    } done(true);
+                }, 1000);
+            }
+        },
+        {
+            type: "input",
+            message: "Please enter a password:\n",
+            name: "password"
+        }
+    ]).then(function(answer) {
+        users.push(answer.username);
+        users.push(answer.password);
+        console.log("Username: " + answer.username + "\n");
+        console.log("Password: " + answer.password + "\n");
+    });
+}
+login();
